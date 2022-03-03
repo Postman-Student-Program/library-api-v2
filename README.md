5edde153f1a0b74de54c4d30d3492007f114616c
clairefro <claire.froelich@gmail.com>
2022-03-03 18:28:58 -0500

    Add schema

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ You will need `Docker` and `docker-compose` ([install](https://docs.docker.com/c
 `npm install`
 
 `npm run dev`
+
+## Updating schema.yaml
+
+`schema.yaml` is the single source of truth used to validate requests and reponses for the API.
+
+**IMPORTANT** Whenever you change `schema.yaml`, run `npm run genJSON` to generate a JSON version of the schema that fastify will use for validation.
+
+If you have changed any routes, be sure to update the locations of the path objects in the server route validations.
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("foo");
+import Fastify from 'fastify'
+import schema from './generated/schema.json'
+
+const fastify = Fastify({ logger: process.env.NODE_ENV === 'development' })
+
+fastify.get('/ping', async (request, reply) => {
+  return 'pong\n'
+})
+
+fastify.get('/books', async (request, reply) => {
+  return 'pong\n'
+})
+
+const PORT = process.env.PORT || 4000
+
+fastify.listen(PORT, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
